1e0e799ede6d1c7f33f97cc7a0fbf486a8a1a522
thedevbrian1 <brayomwas95@gmail.com>
2025-11-23 12:50:17 +0300

    Enrolling user to a course wip..

--- app/models/course.ts
+++ app/models/course.ts
@@ -14,3 +14,22 @@ export async function getCourses() {
 
   return { courses };
 }
+
+export async function enrolToCourse(userId: string) {
+  let reqBody = JSON.stringify({ user_id: userId });
+
+  let res = await fetch(
+    `http://wm-hack-env.eba-pyegadkw.us-west-2.elasticbeanstalk.com/api/v1/courses/{3a86da16-22f1-4453-ab1e-62d107a42767}/enroll/`,
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        // Host: "wm-hack-env.eba-pyegadkw.us-west-2.elasticbeanstalk.com",
+      },
+      body: reqBody,
+    }
+  );
+  console.log(`${res}`);
+  let courses = await res.json();
+}
